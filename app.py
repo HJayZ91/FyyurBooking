8570b4c21cb18e6c09f7f03e2834239118c53464
HJayZ91 <huajie.zhuang@gmail.com>
2022-04-07 13:54:52 -0500

    completed 1st migration

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ class Artist(db.Model):
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
-db.create_all()
+# db.create_all()
 
 #----------------------------------------------------------------------------#
 # Filters.
